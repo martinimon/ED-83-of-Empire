af063b68e38dd83c8833f870033c6e3bd870f193
elliott <elliott.ortmann@protonmail.com>
2024-06-19 19:34:37 +0930

    Cleaned up switch statements

--- EdgeOfEmpireBot/EdgeOfEmpireBot/MessageHandlers/SteamHandler/SteamMessageHandler.cs
+++ EdgeOfEmpireBot/EdgeOfEmpireBot/MessageHandlers/SteamHandler/SteamMessageHandler.cs
@@ -5,35 +5,43 @@ namespace HK47.MessageHandlers;
 
 public class SteamMessageHandler(ISteamService steamService, IDataService dataService, IMessageService messageService) : ISteamMessageHandler
 {
-    /// </inheritdoc>
+    /// <inheritdoc/>
     public async Task ProcessCommand(string userInput)
     {
         var commandParams = userInput.Split(' ');
-        var command = commandParams!.First().ToLower();
+        var command = commandParams[0].ToLower();
         switch (command)
         {
-            case "requestgame":
-            case "gr":
-                try
-                {
-                    var appId = commandParams[1];
-                    var game = await steamService.RetrieveGameFromSteam(appId);
-                    await dataService.WriteGameToFile(game);
-                    await messageService.SendMessage($"```[Statement]: {game.Name} was added to the list.```");
-                }
-                catch (Exception ex)
-                {
-                    await messageService.SendMessage($"[Error]: Game not added due to the following:\n{ex.Message}");
-                }
+            case "requestGame":
+                await RecordGameRequest(commandParams[1]);
                 break;
-            case "games":
-                var (msg, updatedGames) = await steamService.GetGamePrices();
-                if (updatedGames.Count != 0) { await dataService.UpdateGames(updatedGames); }
-                await messageService.SendMessage($"{msg}");
+            case "displayGames":
+                await DisplayCurrentGames();
                 break;
             default:
-                await messageService.SendMessage("[Error]: Invalid command detected...");
+                await messageService.SendMessage("[Error]: You broke the router so badly that you ended up in a place thats unreachable good job jack ass!");
                 break;
         }
+    }
+
+    private async Task RecordGameRequest(string appId)
+    {
+        try
+        {
+            var game = await steamService.RetrieveGameFromSteam(appId);
+            await dataService.WriteGameToFile(game);
+            await messageService.SendMessage($"```[Statement]: {game.Name} was added to the list.```");
+        }
+        catch (Exception ex)
+        {
+            await messageService.SendMessage($"[Error]: Game not added due to the following:\n{ex.Message}");
+        }
+    }
+
+    private async Task DisplayCurrentGames()
+    {
+        var (msg, updatedGames) = await steamService.GetGamePrices();
+        if (updatedGames.Count != 0) { await dataService.UpdateGames(updatedGames); }
+        await messageService.SendMessage($"{msg}");
     }
 }
